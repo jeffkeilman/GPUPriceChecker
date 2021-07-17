5aa0f6549f25c43b72eea2a740d99370be2c2967
Jeff Keilman <jkeilman93@gmail.com>
2021-07-16 21:34:20 -0500

    Add puppeteer and GetDOM wrapper, basic functions for Newegg, tester script

---
+++ src/GetDOM/GetDOM.js
@@ -0,0 +1,18 @@
+const Puppeteer = require("puppeteer");
+
+class GetDOM {
+    static async getDOM (url) {
+        const browser = await Puppeteer.launch();
+        try {
+            const page = await browser.newPage();
+            await page.goto(url);
+            return await page.content();
+        } catch (err) {
+            console.error(err);
+        } finally {
+            await browser.close();
+        }
+    }
+}
+
+module.exports = GetDOM;
