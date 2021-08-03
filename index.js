b1339cf73b8b748757223670460fabcdc9b0e297
Jeff Keilman <jkeilman93@gmail.com>
2021-08-02 20:58:54 -0500

    Remove comments and whitespace

--- index.js
+++ index.js
@@ -25,18 +25,3 @@ const main = () => {
 client.on('ready', main)
 
 client.login(process.env.TOKEN)
-
-
-// () => {
-//   setInterval(() => {
-//     // TODO: Add other sites and handling for multiple cards
-//     const newegg = new Newegg('GTX 1660 Super')
-//     newegg.getCheapestProductAllPages().then((res) => {
-//       client.channels.cache.get(process.env.CHANNEL_ID)
-//         .send(`The cheapest ${newegg.getGPUName()} is: $${res.price}.\nCheck it out here: ${res.link}`)
-//     }).catch((err) => {
-//       console.error(err)
-//     })
-//   }, checkPricesInterval)
-
-
