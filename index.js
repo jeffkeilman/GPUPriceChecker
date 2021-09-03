cda76de2b4323811d34a5ca373569ad67fe8f198
Jeff Keilman <jkeilman93@gmail.com>
2021-09-03 13:48:34 -0500

    Refactor index into class

--- index.js
+++ index.js
@@ -1,77 +1,83 @@
 const Discord = require('discord.js')
-const client = new Discord.Client()
-
 const Newegg = require('./src/Newegg/Newegg')
 
 const FileSystem = require('./src/FileSystem/FileSystem')
 
-const arrayPush = (arr, itemsToPush) => {
-  itemsToPush.forEach(item => {
-    arr.push(item)
-  })
-}
+class Index {
+  constructor () {
+    this.client = new Discord.Client()
+  }
 
-const processBatch = async (promises, cheapestCards) => {
-  arrayPush(cheapestCards, await Promise.all(promises))
-  promises.splice(0, promises.length)
-}
+  init () {
+    this.client.on('ready', this.main.bind(this))
+    this.client.login(process.env.TOKEN)
+  }
 
-const main = async () => {
-  console.log('GPU Price Checker started!')
-  const graphicsCardList = FileSystem.getGraphicsCardList('GPUList/GPUList.txt')
-  const channel = client.channels.cache.get(process.env.CHANNEL_ID)
-  let running = false
-  setInterval(async () => {
-    const start = new Date()
-    console.log('Started at:', start.toISOString())
-    if (running) {
-      console.log('Already running, skipping!')
-      return
-    }
-    running = true
+  arrayPush (arr, itemsToPush) {
+    itemsToPush.forEach(item => {
+      arr.push(item)
+    })
+  }
 
-    const cheapestCards = []
-    const batchSize = Number(process.env.BROWSER_BATCH_SIZE) || 0
-    const cheapestCardsPromises = []
-    let batch = 0
-    for (let x = 0; x < graphicsCardList.length; x++) {
-      // batchSize of 0 means no batching, otherwise check if browser batch size reached
-      if (batchSize !== 0 && batch === batchSize) {
-        await processBatch(cheapestCardsPromises, cheapestCards)
-        batch = 0
+  async processBatch (promises, cheapestCards) {
+    this.arrayPush(cheapestCards, await Promise.all(promises))
+    promises.splice(0, promises.length)
+  }
+
+  async main () {
+    console.log('GPU Price Checker started!')
+    const graphicsCardList = FileSystem.getGraphicsCardList('GPUList/GPUList.txt')
+    const channel = this.client.channels.cache.get(process.env.CHANNEL_ID)
+    let running = false
+    setInterval(async () => {
+      const start = new Date()
+      console.log('Started at:', start.toISOString())
+      if (running) {
+        console.log('Already running, skipping!')
+        return
+      }
+      running = true
+
+      const cheapestCards = []
+      const batchSize = Number(process.env.BROWSER_BATCH_SIZE) || 0
+      const cheapestCardsPromises = []
+      let batch = 0
+      for (let x = 0; x < graphicsCardList.length; x++) {
+        // batchSize of 0 means no batching, otherwise check if browser batch size reached
+        if (batchSize !== 0 && batch === batchSize) {
+          await this.processBatch(cheapestCardsPromises, cheapestCards)
+          batch = 0
+        }
+
+        const newegg = new Newegg(graphicsCardList[x])
+        cheapestCardsPromises.push(newegg.getCheapestProductAllPages())
+        batch++
       }
 
-      const newegg = new Newegg(graphicsCardList[x])
-      cheapestCardsPromises.push(newegg.getCheapestProductAllPages())
-      batch++
-    }
+      // if there are any left to process at the end, wait for them
+      if (batch > 0) {
+        await this.processBatch(cheapestCardsPromises, cheapestCards)
+      }
 
-    // if there are any left to process at the end, wait for them
-    if (batch > 0) {
-      await processBatch(cheapestCardsPromises, cheapestCards)
-    }
+      let channelMessage = ''
+      cheapestCards.forEach(card => {
+        channelMessage += `The cheapest ${card.name} is: $${card.price}.\nCheck it out here: ${card.link}\n\n`
+      })
+      channelMessage = channelMessage.substring(0, channelMessage.length - 2)
+      // channel.send(channelMessage)
+      console.log('Channel Message:', channelMessage)
 
-    let channelMessage = ''
-    cheapestCards.forEach(card => {
-      channelMessage += `The cheapest ${card.name} is: $${card.price}.\nCheck it out here: ${card.link}\n\n`
-    })
-    channelMessage = channelMessage.substring(0, channelMessage.length - 2)
-    channel.send(channelMessage)
-
-    const end = new Date()
-    console.log('Ended at:', end.toISOString())
-    console.log('Time elapsed in seconds:', (end.getTime() - start.getTime()) / 1000)
-    running = false
-  }, Number(process.env.INTERVAL))
+      const end = new Date()
+      console.log('Ended at:', end.toISOString())
+      console.log('Time elapsed in seconds:', (end.getTime() - start.getTime()) / 1000)
+      running = false
+    }, Number(process.env.INTERVAL))
+  }
 }
 
-client.on('ready', main)
-
-client.login(process.env.TOKEN)
-
-// for testing
-module.exports = {
-  arrayPush,
-  processBatch,
-  main
+if (require.main === module) {
+  const index = new Index()
+  index.init()
 }
+
+module.exports = Index
